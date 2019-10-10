fd17c8a25f63041aeeb274d29ca793b01817456d
Deon Odendaal <deon.odendaal@protonmail.com>
2019-10-10 15:25:45 +0100

    prettier conflict

--- pagination.js
+++ pagination.js
@@ -3,7 +3,7 @@ var spec = {
   selectPage: 0 //if the page selected is 1 the prevButton is disabled if the page selected is the last page the nextButton is disabled
 };
 
-spec.selectPage = getUrlParameters("page");
+spec.selectPage = getUrlParameters('page');
 
 //console.log(blogPosts.length);
 var pages = blogPosts.length / spec.numPerPage;
@@ -16,21 +16,21 @@ function init() {
   numberOfPages = Math.ceil(blogPosts.length / spec.numPerPage);
 
   //var i;
-  let buttonList = document.getElementById("buttonList"); // Pagination buttons [Previous, 1, 2, 3, Next] <ul> ... </ul>
-  let refNode = document.getElementById("nextButton"); // "Next" button
+  let buttonList = document.getElementById('buttonList'); // Pagination buttons [Previous, 1, 2, 3, Next] <ul> ... </ul>
+  let refNode = document.getElementById('nextButton'); // "Next" button
 
   for (let i = 0; i < numberOfPages; i++) {
-    let li = document.createElement("li"); // <li> </li>
-    li.classList.add("page-item"); // <li class="page-item"></li>
+    let li = document.createElement('li'); // <li> </li>
+    li.classList.add('page-item'); // <li class="page-item"></li>
 
     if (i == spec.selectPage - 1) {
       // If current page is the selectedPage
-      li.classList.add("active"); // <li class="page-item active"></li>
+      li.classList.add('active'); // <li class="page-item active"></li>
     }
 
-    let a = document.createElement("a"); // <a> </a>
-    a.classList.add("page-link"); // <a class="page-link"> </a>
-    a.href = "posts.html?page=" + (i + 1); // <a href="posts.html?page=i+1" class="page-link"> </a>
+    let a = document.createElement('a'); // <a> </a>
+    a.classList.add('page-link'); // <a class="page-link"> </a>
+    a.href = 'posts.html?page=' + (i + 1); // <a href="posts.html?page=i+1" class="page-link"> </a>
 
     let aText = document.createTextNode(i + 1); //  "1"
     a.appendChild(aText); // <a href="posts.html?page=i+1" class="page-link"> 1 </a>
@@ -39,22 +39,22 @@ function init() {
     buttonList.insertBefore(li, refNode); // <ul>.... <li id="nextButton">...</li></ul>
   }
 
-  var prevButton = document.getElementById("prevButton");
-  var prevButtonA = prevButton.getElementsByTagName("a")[0];
-  prevButtonA.href = "posts.html?page=" + (spec.selectPage - 1);
+  var prevButton = document.getElementById('prevButton');
+  var prevButtonA = prevButton.getElementsByTagName('a')[0];
+  prevButtonA.href = 'posts.html?page=' + (spec.selectPage - 1);
 
-  var nextButton = document.getElementById("nextButton");
-  var nextButtonA = nextButton.getElementsByTagName("a")[0];
-  nextButtonA.href = "posts.html?page=" + (Number(spec.selectPage) + 1);
+  var nextButton = document.getElementById('nextButton');
+  var nextButtonA = nextButton.getElementsByTagName('a')[0];
+  nextButtonA.href = 'posts.html?page=' + (Number(spec.selectPage) + 1);
 
   // Get the value "disable" of the onclick attribute of a button element Previous and Next
   if (spec.selectPage == 1) {
     // when the value is equal the page "1" them previous button is disabled
 
-    prevButton.classList.add("disabled"); // <li id="prevButton" class="... disabled"></li>
+    prevButton.classList.add('disabled'); // <li id="prevButton" class="... disabled"></li>
   }
   if (spec.selectPage == numberOfPages) {
     //when the value is equal the "last page" (now is page 5) them next button is disabled
-    nextButton.classList.add("disabled");
+    nextButton.classList.add('disabled');
   }
 }
