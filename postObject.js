a8ed7496691316b9fbb590a011450ad338f2ed84
Deon Odendaal <deon.odendaal@protonmail.com>
2019-10-11 10:58:17 +0100

    change author name to Wildo Coda

--- postObject.js
+++ postObject.js
@@ -1,6 +1,6 @@
 var blogPosts = [
   {
-    author: 'Leanne Graham',
+    author: 'Wildo Coda',
     title: 'Great sights in Brussels',
     imageUrl: 'https://source.unsplash.com/m39OKAexaqo',
     imageAlt: 'Belgian cities',
@@ -9,7 +9,7 @@ var blogPosts = [
       "Wake up human for food at 4am leave dead animals as gifts, yet kitty scratches couch bad kitty yet human clearly uses close to one life a night no one naps that long so i revive by standing on chestawaken!. Leave dead animals as gifts claw drapes jump off balcony, onto stranger's head have secret plans so scratch pee on walls it smells like breakfast and litter box is life. Demand to have some of whatever the human is cooking, then sniff the offering and walk away a nice warm laptop for me to sit on run as fast as i can into another room for no reason. Go into a room to decide you didn't want to be in there anyway tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad for i’m so hungry i’m so hungry but ew not for that , but man running from cops stops to pet cats, goes to jail. Demand to be let outside at once, and expect owner to wait for me as i think about it chase after silly colored fish toys around the house with tail in the air. Sleep on keyboard shove bum in owner's face like camera lens. Plop down in the middle where everybody walks. Lay on arms while you're using the keyboard human is washing you why halp oh the horror flee scratch hiss bite, i hate cucumber pls dont throw it at me proudly present butt to human love you, then bite you for cat dog hate mouse eat string barf pillow no baths hate everything, or behind the couch."
   },
   {
-    author: 'Ervin Howell',
+    author: 'Wildo Coda',
     title: 'Visit to the Atomium',
     imageUrl: 'https://source.unsplash.com/5PEy9UraJ5c',
     imageAlt: 'Atomium',
@@ -18,7 +18,7 @@ var blogPosts = [
       "Well the first thing you know ol' Jeds a millionaire. Kinfolk said Jed move away from there! The mate was a mighty sailin' man the Skipper brave and sure. Five passengers set sail that day for a three hour tour a three hour tour. Believe it or not I'm walking on air. I never thought I could feel so free. Flying away on a wing and a prayer. Who could it be? Believe it or not its just me. Doin' it our way. There's nothing we wont try. Never heard the word impossible. This time there's no stopping us. Well we're movin' on up to the east side to a deluxe apartment in the sky. Fish don't fry in the kitchen and beans don't burn on the grill. Took a whole lotta tryin' just to get up that hill. Doin' it our way. There's nothing we wont try. Never heard the word impossible. This time there's no stopping us."
   },
   {
-    author: 'Clementine Bauch',
+    author: 'Wildo Coda',
     title: 'Buildings buildings buildings...',
     imageUrl: 'https://source.unsplash.com/zMbxBDg4qBc',
     imageAlt: 'Arial view of buildings',
@@ -27,7 +27,7 @@ var blogPosts = [
       'Lorem ipsum dolor amet shoreditch flannel literally blue bottle chambray seitan occupy marfa. Etsy PBR&B tousled, shabby chic semiotics blue bottle aesthetic pitchfork dreamcatcher ugh. Yuccie narwhal XOXO post-ironic yr jianbing cold-pressed heirloom scenester. Succulents taxidermy PBR&B single-origin coffee squid cray tumblr ethical. Flexitarian stumptown freegan twee cardigan. Distillery authentic seitan, kombucha banh mi drinking vinegar kinfolk vinyl roof party franzen shoreditch snackwave craft beer. Adaptogen biodiesel migas swag beard glossier vinyl. Literally pork belly enamel pin, typewriter iceland activated charcoal 8-bit biodiesel edison bulb bitters. Tattooed YOLO four loko migas adaptogen, letterpress ethical typewriter poke unicorn DIY.'
   },
   {
-    author: 'Chelsey Dietrich',
+    author: 'Wildo Coda',
     title: 'Brussels marathon',
     imageUrl: 'https://source.unsplash.com/NPFu4GfFZ7E',
     imageAlt: 'Brussels marathon runners',
@@ -36,7 +36,7 @@ var blogPosts = [
       'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.'
   },
   {
-    author: 'Dennis Schulist',
+    author: 'Wildo Coda',
     title: 'Fuse Brussels',
     imageUrl: 'https://source.unsplash.com/kZ5lgy2xRvs',
     imageAlt: 'fuse brussels',
@@ -45,7 +45,7 @@ var blogPosts = [
       'Gummies jelly beans bonbon bonbon muffin dragée halvah pie gummi bears. Cupcake jelly soufflé chocolate bar candy chupa chups fruitcake. Cheesecake jelly-o lemon drops. Lemon drops sweet topping chocolate bar cheesecake tiramisu.Chocolate bar gingerbread cake fruitcake danish. Tootsie roll powder pastry croissant gummi bears sesame snaps wafer dessert brownie. Danish pastry pastry marzipan pudding. Apple pie caramels lollipop.Tart sweet chupa chups tiramisu muffin. Gingerbread chocolate bar topping tart cotton candy soufflé gingerbread marzipan tiramisu. Gummies apple pie cheesecake muffin muffin jujubes. Pie oat cake wafer dragée.'
   },
   {
-    author: 'Kurtis Weissnat',
+    author: 'Wildo Coda',
     title: 'So many lines',
     imageUrl: 'https://source.unsplash.com/5Ek7bmlylnQ',
     imageAlt: 'so many lines',
@@ -54,7 +54,7 @@ var blogPosts = [
       "Let's put him in there. Dammit, Doc, why did you have to tear up that letter? If only I had more time. Wait a minute, I got all the time I want I got a time machine, I'll just go back and warn him. 10 minutes oughta do it. Time-circuits on, flux-capacitor fluxing, engine running, alright. No, no no no no, c'mon c'mon. C'mon c'mon, here we go, this time. Please, please, c'mon. The appropriate question is, weren't the hell are they. Einstein has just become the world's first time traveler. I sent him into the future. One minute into the future to be exact. And at exactly 1:21 a.m. we should cat h up with him and the time machine. Doc, you gotta help me. you were the only one who knows how your time machine works. Just finishing up the second coat now."
   },
   {
-    author: 'Nicholas Runolfsdottir',
+    author: 'Wildo Coda',
     title: 'Brussels Lights',
     imageUrl: 'https://source.unsplash.com/RE7rjVjjJuM',
     imageAlt: 'brussels lights',
@@ -63,7 +63,7 @@ var blogPosts = [
       'Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before. Many say exploration is part of our destiny, but it’s actually our duty to future generations and their quest to ensure the survival of the human species.'
   },
   {
-    author: 'Glenna Reichert',
+    author: 'Wildo Coda',
     title: 'Minimal Brussels',
     imageUrl: 'https://source.unsplash.com/JI01fn0U7Cg',
     imageAlt: 'minimal brussels',
